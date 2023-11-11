d0ce9fe0f3abcdb9891cb64bfd2c627a3080f115
Genokarma <135817703+Genokarma@users.noreply.github.com>
2023-11-11 15:59:12 +0530

    Script_DeepCAT.sh

--- Script_DeepCAT.sh
+++ Script_DeepCAT.sh
@@ -4,7 +4,7 @@
 var1="Output"
 # If raw TCR repertoire sequencing data are not available 
 # please use our Sample Data as an example 
-#python DeepCAT_modif.py $var3  $var4   
+#python3 DeepCAT_modif.py $var3  $var4   
 var2="iSMART_results"
 var3="DeepCAT_CHKP"
 #
@@ -17,8 +17,8 @@ else
        if [ ! -d $var2 ]; then
          mkdir $var2
        fi
-       python PrepareAdaptiveFile.py ${args[1]} $var1
-       python iSMARTm.py -d $var1 -o $var2  
+       python3 PrepareAdaptiveFile.py ${args[1]} $var1
+       python3 iSMARTm.py -d $var1 -o $var2  
    else
        echo "Error! The" ${args[1]} "directory is empty"
        exit 1
@@ -26,5 +26,5 @@ else
  elif [ ${args[0]} == '-t' ]; then
     var2=${args[1]}
  fi  
- python DeepCAT.py $var2 $var3 ${args[0]}
+ python3 DeepCAT.py $var2 $var3 ${args[0]}
 fi
